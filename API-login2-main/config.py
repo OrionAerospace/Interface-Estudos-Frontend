dec74ca7b37a16b2d9edef9505ee4fe342c814bc
Arthur Baldoqui <arthur.b.bergamo@gmail.com>
2023-11-16 21:00:35 -0300

    A database é setada agora como uma variável de desenvolvimento

--- API-login2-main/config.py
+++ API-login2-main/config.py
@@ -1,9 +1,10 @@
 import string
 import random
+from os import environ
 
 randon_str = string.ascii_letters + string.digits + string.ascii_uppercase
 key = ''.join(random.choice(randon_str) for i in range(12))
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Orion123!@localhost/banco_api'
+SQLALCHEMY_DATABASE_URI = environ.get('DB_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = key
